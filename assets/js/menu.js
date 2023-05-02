876d7777ea227fc25e6d51e753d2bb3956a69dd7
Ben Sales <adebsa2401@gmail.com>
2023-05-02 14:01:59 +0100

    feat: Add responsive breakpoint at 1008px

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,25 @@
 // open menu popup
 document.getElementById('menu-icon').addEventListener('click', () => {
+  document.getElementById('mobile-menu').classList.remove('animate__fadeOutRight');
+  document.getElementById('mobile-menu').classList.add('animate__fadeInRight');
+  document.querySelectorAll('.menu-item').forEach((item, index) => {
+    if (index % 2 === 0) {
+      item.classList.add('animate__fadeInLeft');
+    } else {
+      item.classList.add('animate__fadeInRight');
+    }
+  });
   document.getElementById('mobile-menu').style.display = 'block';
 });
 
 // close menu popup
 Array.from(document.getElementsByClassName('menu-item')).forEach((element) => {
   element.addEventListener('click', () => {
-    document.getElementById('mobile-menu').style.display = 'none';
+    document.getElementById('mobile-menu').classList.remove('animate__fadeInRight');
+    document.getElementById('mobile-menu').classList.add('animate__fadeOutRight');
+    setTimeout(() => {
+      document.getElementById('mobile-menu').style.display = 'none';
+    }, 500);
   });
 });
 
